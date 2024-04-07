d28011804a0573df98b62ee68f3e014f050a3c16
tanvirgb <tspramanik123@gmail.com>
2024-04-07 21:07:05 +0600

    Added logout feature

---
+++ Online-Voting-System-Project/view/logout.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logout</title>
+    <link rel="stylesheet" href="styles_8.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>Logout</h1>
+    </header>
+
+    <div class="container">
+        <h2>Are you sure you want to log out?</h2>
+        <p>This action will log you out of the system.</p>
+        <p><a href="javascript:history.go(-1)" class="go-back-link">Go Back</a></p>
+        <p><a href="../controller/logout_process.php" class="logout-link">Logout</a></p>
+
+    </div>
+
+    <footer>
+        &copy; 2024 Online Voting System | Created by Md. Tanvir Siddique Pramanik
+    </footer>
+
+</body>
+
+</html>
