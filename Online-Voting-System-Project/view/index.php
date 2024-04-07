61807cd55b0bbb60613a57c3e0600bfa56f38cba
tanvirgb <tspramanik123@gmail.com>
2024-04-07 20:25:10 +0600

    Added home feature

---
+++ Online-Voting-System-Project/view/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Online Voting System</title>
+  <link rel="stylesheet" href="styles_0.css" />
+</head>
+
+<body>
+  <header>
+    <h1>Online Voting System</h1>
+    <p>By <b>Md. Tanvir Siddique Pramanik</b></p>
+  </header>
+
+  <nav>
+    <a href="index.php">Home</a>
+    <a href="login.php">Login</a>
+    <a href="registration.php">Registration</a>
+    <a href="about.php">About</a>
+  </nav>
+
+  <div class="container">
+    <h2>Welcome to Our Website!</h2>
+  </div>
+
+  <footer>
+    &copy; 2024 Online Voting System | Created by Md. Tanvir Siddique Pramanik
+  </footer>
+  <img class="background-image" src="./vote-1804596_1920.jpg" alt="Background Image" />
+</body>
+
+</html>
